84488a5e7666cef6a2af49dfd4dc9f534f6f8a75
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-02-16 23:11:34 +0700

    Create Sorter.java

---
+++ Sorter.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Sorter {
+
+    public static void main(String[] args) {
+       if (args.length != 2) {
+            System.out.print("Bad number of args");
+            return;
+        }
+       
+        Reader reader = null;
+        Writer writer = null;
+        try {
+            reader = new InputStreamReader(new FileInputStream(args[0]));
+            writer = new OutputStreamWriter(new FileOutputStream(args[1]));
+             
+            CSVAnalyzer analyzer = new CSVAnalyzer(reader); 
+            new CSVWriter(writer, analyzer);        
+        } 
+        catch (IOException exception){
+            System.err.print("Error while analyzing file" + exception.getLocalizedMessage());
+        } 
+        finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } 
+                catch (IOException exception) {
+                    exception.printStackTrace(System.err);
+                }
+            }
+            if (null != writer) {
+                try {
+                    writer.close();
+                } 
+                catch (IOException exception) {
+                    exception.printStackTrace(System.err);
+                }
+            }
+        }
+    }
+}
+
